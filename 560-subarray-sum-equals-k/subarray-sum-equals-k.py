40ef49ab0019f8d030d7b5070354a36331fa6e73
Paramanand Kumar <49456865+iamparamanand@users.noreply.github.com>
2024-02-22 00:33:19 +0530

    Time: 244 ms (24.28%) | Memory: 19.6 MB (63.80%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -3,16 +3,11 @@ class Solution:
         count = 0
         map = {}
         sum_val = 0
-
         for num in nums:
             sum_val += num
-
             if sum_val == k:
                 count += 1
-
             if sum_val - k in map:
                 count += map[sum_val - k]
-
             map[sum_val] = map.get(sum_val, 0) + 1
-
         return count
