8351d2134632422256ece3d882647dbb974546e1
Paramanand Kumar <49456865+iamparamanand@users.noreply.github.com>
2024-03-28 00:33:17 +0530

    Time: 55 ms (28.64%) | Memory: 16.9 MB (91.44%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,23 +1,14 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # return sorted(s.lower()) == sorted(t.lower())
-        if(len(t) != len(s)):
-            return False
-        dictT,dictS = {},{}
-        for i in range(len(t)):
-            dictT[t[i]] = 1+ dictT.get(t[i],0)
-            dictS[s[i]] = 1+ dictS.get(s[i],0)
-
-        return dictT == dictS
-
-#Another solution using sorted function
-        # len_s = len(s)
-        # len_t = len(t)
-        # s = sorted(s) 
-        # t = sorted(t)
-        # if len_s == len_t:
-        #     for i in range(0,len_s):
-        #         while s[i] != t[i]:
-        #             return False
-        
-        #     return True
+        seen_s, seen_t = {}, {}
+        if len(s) == len(t):
+            for i in range(len(s)):
+                if s[i] not in seen_s:
+                    seen_s[s[i]] = 1
+                seen_s[s[i]] += 1
+                if t[i] not in seen_t:
+                    seen_t[t[i]] = 1
+                seen_t[t[i]] += 1
+            res = all([seen_s.get(key) == value for key, value in seen_t.items()])
+            return res
+        return False
